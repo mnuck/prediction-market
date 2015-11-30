b6aefacfd20b2fb30d646d067a10cea9ca902bb9
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2015-11-29 23:31:50 -0500

    Move guard ifdef to correct place

--- Book/Logger.h
+++ Book/Logger.h
@@ -1,12 +1,13 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
 #define BOOST_LOG_DYN_LINK 1
 
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/trivial.hpp>
 
-#ifndef LOGGER_H
-#define LOGGER_H
- 
 #define LOG(severity) BOOST_LOG_SEV(logger::get(),boost::log::trivial::severity)
+
 BOOST_LOG_GLOBAL_LOGGER(logger, boost::log::sources::severity_logger_mt<boost::log::trivial::severity_level>)
 
 bool SetLogLevel(const unsigned int level);
